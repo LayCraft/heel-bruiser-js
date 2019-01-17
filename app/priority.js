273bebd7f062dd1d913165b02b887f5cb1707b11
Alice <alice@portable.local>
2019-01-17 07:37:18 -0800

    add idea for return

--- app/priority.js
+++ app/priority.js
@@ -13,6 +13,9 @@ module.exports.buildPriority = (request, board) =>{
     //assess other snake food need. Can I get a hungrier snake's food first?
     //can I trap another snake in a small space?
     //
-
-    return "LRLRL"
+    // this should return a list of strategy objects that we can filter/reduce later
+    let strat = [
+        {direction:"left", name:"foodHoarding", moves:4, risk:20, confidence:100},
+        {direction:"left", name:"spanner", moves:10, risk:30, confidence:6}]
+    return strat
 }
