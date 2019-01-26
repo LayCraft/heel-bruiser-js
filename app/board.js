393b5e342178e5c51619c0f67d3e74233f96a69d
Alice <alice@portable.local>
2019-01-25 22:49:53 -0800

    error on duplicate objects vertically

--- app/board.js
+++ app/board.js
@@ -1,21 +1,50 @@
 module.exports = class Board{
 	constructor(blob){
 		//the constructor is a function that builds a basic board from the data
+		//Meta about board
 		this.id = blob.you.id
 		this.width = blob.board.width
 		this.height = blob.board.height
-		//make an empty 2d array
-		this.board = Array(blob.board.height).fill(Array(blob.board.width).fill({}))
-		this.getOrth(blob.you.body[0])
+		//make an empty 2d array to hold the POIs
+		this.board = Array(blob.board.height).fill(Array(blob.board.width).fill(null))
+		// console.log(blob)
+		
+		//write food onto the board
+		// blob.board.food.forEach(bite=>{
+		// 	//all food should be marked on the board
+		// 	bite.food = true
+		// 	this.setSpace(bite)
+		// })
+		this.setSpace({x:2,y:2,bork:"bork"})
+		console.log(this.board)
 	}
+
 	getSpace(poi) {
 		//What is at the location?
 		return this.board[poi.y][poi.y]
 	}
+
 	setSpace(poi){
 		//Set the location
-		this.board[poi.y][poi.x] = poi
+		//when a location is null set it to whatever is sent
+		if(!this.board[poi.y][poi.x]){
+			console.log("Null found")
+			this.board[poi.y][poi.x] = poi
+		} else {
+			console.log("Null not found")
+
+			//collect the existing space
+			let space = this.getSpace(poi)
+			// every key in the sent item should be added to the space
+			poi.keys.forEach(key => {
+				//write the keys into space
+				space[key] = poi[key]
+			})
+			//put the space back into the board
+			this.setSpace(space)
+		}
 	}
+
 	getOrth(poi){
 		//return an array of all poi orthoganaly around the supplied one
 		return [
@@ -25,7 +54,7 @@ module.exports = class Board{
 			{x: poi.x, y: poi.y-1},
 			{x: poi.x, y: poi.y+1},
 		].filter(poi=>{
-			//remove all of the invalid elements
+			//remove all of the elements that are outside the board parameters
 			if(poi.x<0 || poi.y<0 || poi.y===this.height || poi.x===this.width){
 					return false
 			} else return true
