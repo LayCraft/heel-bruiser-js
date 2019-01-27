0c1312908c8806eb1bd3826c986c24db674a42e1
Alice <alice@portable.local>
2019-01-26 19:35:29 -0800

    tail danger when head near food

--- app/board.js
+++ app/board.js
@@ -9,6 +9,8 @@ module.exports = class Board{
 		//the constructor is a function that builds a basic board from the data
 		//Meta about board
 		this.myId = blob.you.id
+		// this.myLength = blob.you.
+		this.myLength = blob.you.body.length
 		this.width = blob.board.width
 		this.height = blob.board.height
 		//make an empty 2d array to hold the POIs
@@ -68,6 +70,7 @@ module.exports = class Board{
 		//handle all logic for placing snakes here
 		// console.log(snake)
 		snake.body.forEach((poi, i)=>{
+			//-----Board facts below
 			if (i===0){
 				//is a head
 				//snake head holds meta info
@@ -76,6 +79,7 @@ module.exports = class Board{
 				poi['size']=snake.body.length
 				poi['tailspace']=snake.body[snake.body.length-1]
 				poi['id']=snake.id
+				poi['traversable']=false
 			}else if(i==snake.body.length-1){
 				// is a tail
 				poi['tail']=true
@@ -86,10 +90,37 @@ module.exports = class Board{
 				poi['body']=true
 				//all non body spaces know the head
 				poi['headspace']=snake.body[0]
+				poi['traversable']=false
 			}
-			//set the space on the board
+			//set the space on the board with the facts
 			this.setSpace(poi)
+
+			//-----Danger identification below-----------
+			// "Opinions" on what is dangerous
+			let orthPoi = this.getOrth(poi).map(p=>this.getSpace(p))
+			//is a head, is not me, is an equal or longer snake,
+			if(i===0 && snake.id!=this.myId && snake.body.length>=this.myLength){
+				// mark orthoganal points by head as threats
+				orthPoi.forEach(p=>{
+					p['danger'] = true
+					this.setSpace(p)
+				})
+			}
+			//is a head, is not me, has at least one food near the head
+			if(i===0 && orthPoi.filter(p=>{ return p.food })){
+				//mark tail a threat of expansion if head is beside food. (do not traverse)
+				poi.tailspace['danger']=true
+				this.setSpace(poi)
+			}
 		})
+	}
+	
+	traversableArea(poi){
+		//starting at this point how many traversable spaces are there?
+		//loop
+			//get orthoganal points from element in unchecked list
+			//add unvisited traversable points to the unchecked list 
+			//add all points that are not traversable to the checked list
 	}
 
 	print(){
@@ -101,14 +132,14 @@ module.exports = class Board{
 					let f = ' ' //filler character should change if traversable
 					let xinfo = ''
 					//write the spaces
-					if (x.head) xinfo = xinfo + '✅'
-					if (x.food) xinfo = xinfo + '🐿️'
-					if (x.tail) xinfo = xinfo + '❇️'
-					if (x.body) xinfo = xinfo + '❎'
-					if (x.threat) xinfo = xinfo + '🔥'
-					if (!x.traversable) f = '🧱'
+					if (x.food) xinfo = xinfo + 'F'
+					if (x.head) xinfo = xinfo + 'H'
+					if (x.body) xinfo = xinfo + 'B'
+					if (x.tail) xinfo = xinfo + 'T'
+					if (x.danger) xinfo = xinfo + '!'
+					if (!x.traversable) f = '…'
 
-					yrow = yrow + xinfo.padStart(6,f) + '|'
+					yrow = yrow + xinfo.padEnd(6,f) + '|'
 			})
 			console.log(yrow)
 			let line = ''
